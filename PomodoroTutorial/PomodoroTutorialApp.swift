c9c8aaaafdd1171a479aca828d6946c9656eaf2d
mia <miabenn74@gmail.com>
2023-10-04 13:33:16 +1000

    Initial Commit

---
+++ PomodoroTutorial/PomodoroTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroTutorialApp.swift
+//  PomodoroTutorial
+//
+//  Created by Mia on 4/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
